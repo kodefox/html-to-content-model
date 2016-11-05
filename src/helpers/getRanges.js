6d9b0bc7e4430785461b433aa9c20e207f7272a2
Simon Sturmer <sstur@me.com>
2016-11-05 09:12:05 +0700

    Finish getRanges

--- src/helpers/getRanges.js
+++ src/helpers/getRanges.js
@@ -1,15 +1,67 @@
 // @flow
-import type {CharacterMetaList, StyleRange, EntityRange} from '../modelFromElement';
+import type {CharacterMeta, StyleRange, EntityRange, RawEntity} from '../modelFromElement';
 
 type Ranges = {
   inlineStyleRanges: Array<StyleRange>;
   entityRanges: Array<EntityRange>;
 };
 
-function getRanges(characterMeta: CharacterMetaList): Ranges {
+function isEqualEntity(oldEntity: ?RawEntity, newEntity: ?RawEntity): boolean {
+  if (oldEntity == null || newEntity == null) {
+    return (oldEntity == null && newEntity == null);
+  }
+  return (oldEntity.key === newEntity.key);
+}
+
+function getRanges(charMetaList: Array<CharacterMeta>): Ranges {
   let inlineStyleRanges = [];
   let entityRanges = [];
-  // TODO
+  let styleRangeMap = new Map();
+  let prevEntity = null;
+  let prevEntityStartIndex = 0;
+  charMetaList.forEach((charMeta, index) => {
+    let {style, entity} = charMeta;
+    for (let styleName of style) {
+      if (!styleRangeMap.has(styleName)) {
+        styleRangeMap.set(styleName, index);
+      }
+    }
+    for (let [styleName, startIndex] of styleRangeMap) {
+      if (!style.has(styleName)) {
+        inlineStyleRanges.push({
+          style: styleName,
+          offset: startIndex,
+          length: index - startIndex,
+        });
+        styleRangeMap.delete(styleName);
+      }
+    }
+    if (!isEqualEntity(prevEntity, entity)) {
+      if (prevEntity != null) {
+        entityRanges.push({
+          key: prevEntity.key,
+          offset: prevEntityStartIndex,
+          length: index - prevEntityStartIndex,
+        });
+      }
+      prevEntity = entity;
+      prevEntityStartIndex = index;
+    }
+  });
+  for (let [styleName, startIndex] of styleRangeMap) {
+    inlineStyleRanges.push({
+      style: styleName,
+      offset: startIndex,
+      length: charMetaList.length - startIndex,
+    });
+  }
+  if (prevEntity != null) {
+    entityRanges.push({
+      key: prevEntity.key,
+      offset: prevEntityStartIndex,
+      length: charMetaList.length - prevEntityStartIndex,
+    });
+  }
   return {
     inlineStyleRanges,
     entityRanges,
