2dadfced261fbbb8949e81dc628c8565e3a43ea9
Simon Sturmer <sstur@me.com>
2016-11-05 21:28:26 +0700

    Better tests

--- src/__tests__/transform-test.js
+++ src/__tests__/transform-test.js
@@ -2,116 +2,33 @@
 const {describe, it} = global;
 
 import expect from 'expect';
+import fs from 'fs';
+import {join} from 'path';
+
 import {fromHTML} from '../transform';
 
-describe('transform', () => {
+// This separates the test cases in `data/test-cases.txt`.
+const SEP = '\n\n#';
 
-  it('should parse a simple block', () => {
-    let html = `<p>Hello <strong>world</strong></p>`;
-    expect(fromHTML(html)).toEqual({
-      blocks: [
-        {
-          type: 'unstyled',
-          entityNodes: [
-            {
-              entity: null,
-              styleNodes: [
-                {
-                  text: 'Hello ',
-                  styles: null,
-                },
-                {
-                  text: 'world',
-                  styles: ['BOLD'],
-                },
-              ],
-            },
-          ],
-        },
-      ],
+let testCasesRaw = fs.readFileSync(
+  join(__dirname, '..', '..', 'test', 'test-cases.txt'),
+  'utf8',
+);
+
+let testCases = testCasesRaw.slice(1).trim().split(SEP).map((text) => {
+  let lines = text.split('\n');
+  let description = lines.shift().trim();
+  let model = JSON.parse(lines[0]);
+  let html = lines.slice(1).join('\n');
+  return {description, model, html};
+});
+
+describe('stateFromHTML', () => {
+  testCases.forEach((testCase) => {
+    let {description, model, html} = testCase;
+    it(`should render ${description}`, () => {
+      let contentModel = fromHTML(html);
+      expect(contentModel).toEqual(model);
     });
   });
-
-  it('should parse an entity', () => {
-    let html = `Hello <a href="/">world</a>`;
-    expect(fromHTML(html)).toEqual({
-      entityMap: {
-        ['0']: {
-          type: 'LINK',
-          data: {
-            url: '/',
-          },
-        },
-      },
-      blocks: [
-        {
-          type: 'unstyled',
-          entityNodes: [
-            {
-              entity: null,
-              styleNodes: [
-                {
-                  text: 'Hello ',
-                  styles: null,
-                },
-              ],
-            },
-            {
-              entity: '0',
-              styleNodes: [
-                {
-                  text: 'world',
-                  styles: null,
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    });
-  });
-
-  it('should parse an entity with styles', () => {
-    let html = `<b>Hello <a href="/">wo<i>rld</a>`;
-    expect(fromHTML(html)).toEqual({
-      entityMap: {
-        ['0']: {
-          type: 'LINK',
-          data: {
-            url: '/',
-          },
-        },
-      },
-      blocks: [
-        {
-          type: 'unstyled',
-          entityNodes: [
-            {
-              entity: null,
-              styleNodes: [
-                {
-                  text: 'Hello ',
-                  styles: ['BOLD'],
-                },
-              ],
-            },
-            {
-              entity: '0',
-              styleNodes: [
-                {
-                  text: 'wo',
-                  styles: ['BOLD'],
-                },
-                {
-                  text: 'rld',
-                  styles: ['BOLD', 'ITALIC'],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    });
-  });
-
 });
