29e56f058ba127b8846a25a18258a18ad399d69c
Simon Sturmer <sstur@me.com>
2016-11-21 15:12:59 +0700

    Fix issue with <br>

--- src/parseHTML.js
+++ src/parseHTML.js
@@ -20,7 +20,9 @@ export default function parseHTML(html: string): FragmentNode {
     if (currentNode) {
       currentNode.appendChild(newNode);
     }
-    nodeStack.push(newNode);
+    if (!newNode.isSelfClosing) {
+      nodeStack.push(newNode);
+    }
   });
   parser.on('endTag', () => {
     nodeStack.pop();
